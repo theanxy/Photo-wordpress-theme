75d275fd139250eabbe67b92bade3e665e676bec
Wojtek Zajac <wojciech@xhtmlized.com>
2011-05-08 14:55:35 +0200

    switching to local jquery for now

--- wp-content/themes/sandbox/footer.php
+++ wp-content/themes/sandbox/footer.php
@@ -8,6 +8,7 @@
 
 </body>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>-->
+<script src="<?php echo bloginfo('template_directory'); ?>/js/jquery-1.6.min.js"></script>
 <script src="<?php echo bloginfo('template_directory'); ?>/js/main.js"></script>
 </html>
