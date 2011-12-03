90de8f05ce408d288db38f1707fe97a1b50ba846
Wojtek Zając <wojtek@wojtekzajac.com>
2011-12-03 16:55:34 +0100

    header changing on hover

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'photo.wojtekzajac');
 
 /** MySQL database username */
 define('DB_USER', 'root');
