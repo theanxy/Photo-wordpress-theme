9dd6583bbc26cc391dab3e4ecf02185d8615fa45
Wojtek Zając <wojciech@xhtmlized.com>
2012-01-14 15:37:09 +0100

    Updates to home copy

--- wp-content/themes/sandbox/page-home.php
+++ wp-content/themes/sandbox/page-home.php
@@ -13,7 +13,33 @@
 ?>
 <div id="content" class="home">
 	
-<?php get_sidebar('home') ?>
+	<?php /*
+	<section class="bio">
+		<p>Hi!</p>
+		<p>I’m a lead developer at XHTMLized.</p>
+		<p>Photography is my hobby. Learn more about me or check my social media profiles.</p>
+		<p><a href="#">LinkedIn</a></p>
+		<p><a href="#">SlideShare</a></p>
+		<p><a href="#">Facebook</a></p>
+	</section>
+	*/ ?>
+	<img src="/img/Wojtek2.jpg" alt="Wojtek Zając" class="photo">
+	<section class="bio">
+		<p>Hi there! I’m glad you visited<br> my photo gallery.
+
+		<p>I’m 21 and I founded/lead polish office of <a href="http://xhtmlized.com">XHTMLized</a>.
+
+		<p class="more">I’m a <a href="http://slideshare.com/wojciechzajac">public speaker</a> with topics ranging web standards, UX, and typography.
+			
+		<p>I study HCI, listen to <a href="http://last.fm/user/buterux">music</a>, and take photos a lot.
+			
+		<p class="more">I built the first public version of <a href="http://twitter.com">Twitter</a>, and worked on its front-end between 2006—2009.
+			
+		<p class="more">You should <a href="http://twitter.com/theanxy">follow me</a>.
+		
+		<p>Learn more about me.
+	</section>
+
 
 <?php		
 	$images =& get_children( 'post_type=attachment&post_mime_type=image&post_parent='.$currentID.'&orderby=menu_order&order=ASC' );
